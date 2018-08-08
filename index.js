c19af5d718d3db1e70827aed1f2663cb94a3251d
Jon Lansdell <jonlansdell@users.noreply.github.com>
2018-08-08 15:00:02 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ module.exports = {
   website: {
     assets: "./assets",
     js: [
-      "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js",
+      "bootstrap.min.js",
       "sectionx.js"
     ],
     css: [
-      "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css",
+      "bootstrap.min.css",
       "sectionx.css"
     ]
   },
